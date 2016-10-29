960e86f68db930e2d8ee3d66d446adbe9fed096c
tanzeelak <tnkhan8042@gmail.com>
2016-10-29 13:56:35 -0700

    we have a home page

--- app/src/main/java/com/example/tanzeelakhan/swyperight2/Menu.java
+++ app/src/main/java/com/example/tanzeelakhan/swyperight2/Menu.java
@@ -2,12 +2,55 @@ package com.example.tanzeelakhan.swyperight2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
+
+    private TotalText totalText;
+    private Button button4;
+    private Button button5;
+
+    private int plan;
+
+    private TextView remainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        totalText = (TotalText) findViewById(R.id.totalText);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        remainTextView = (TextView) findViewById(R.id.remainTextView);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //what to do on click?
+//                plan = 19;
+//                plan*=10;
+                plan = 19;
+                
+                remainTextView.setText("190");
+
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan = 14;
+                remainTextView.setText("140");
+
+            }
+        });
+
     }
+
+
+    //create function that calculates
 }
