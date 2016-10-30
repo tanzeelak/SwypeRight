e0153c0eff53fbf8ff85ff647a3a795e4af4c420
il-dionigi <brian.dionigi@gmail.com>
2016-10-29 18:51:34 -0700

    Buy Sell Suggestion

--- app/src/main/java/com/example/tanzeelakhan/swyperight2/Menu.java
+++ app/src/main/java/com/example/tanzeelakhan/swyperight2/Menu.java
@@ -1,5 +1,6 @@
 package com.example.tanzeelakhan.swyperight2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -16,15 +17,25 @@ import java.util.*;
 public class Menu extends AppCompatActivity {
 
     private TextView swipesLeftText;
-    private  TextView totalText;
+    private EditText swipesUsed;
     private Button button4;
     private Button button5;
+    private Button btnCalculateSwipes;
 
-    private int plan;
+    private int plan = 0;
+    private int totalSwipes = 0;
+    private int swipesPerDay;
 
     private TextView remainTextView;
 
-    public int swipesLeft(int planType) {
+    public int swipesLeft(int swipesUsed) {
+
+        int swipesLeft = totalSwipes - swipesUsed;
+
+        return swipesLeft;
+    }
+
+    public int swipesShouldHaveUsed(int swipesUsed) {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, 2016);
         cal.set(Calendar.MONTH, Calendar.SEPTEMBER);
@@ -34,19 +45,7 @@ public class Menu extends AppCompatActivity {
 
         long daysSinceSwipesBegan = (now.getTime() - startOfSwipes.getTime())/86400000; // Divide difference in mseconds by the mseconds in a day
 
-        int totalSwipes = 0;
-        int swipesPerDay = 0;
-
-        if (planType == 19) {
-            totalSwipes = 213;
-            swipesPerDay = 3;
-        }
-        if (planType == 14) {
-            totalSwipes = 158;
-            swipesPerDay = 2;
-        }
-
-        return (totalSwipes - ((int) daysSinceSwipesBegan * swipesPerDay));
+        return (int) daysSinceSwipesBegan * swipesPerDay;
     }
 
     @Override
@@ -56,22 +55,50 @@ public class Menu extends AppCompatActivity {
 
         button4 = (Button) findViewById(R.id.button4);
         button5 = (Button) findViewById(R.id.button5);
+        btnCalculateSwipes = (Button) findViewById(R.id.btnCalculateSwipes);
+
         remainTextView = (TextView) findViewById(R.id.swipesLeftText);
+        swipesUsed = (EditText) findViewById(R.id.swipesUsed);
 
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                button4.setTextColor(Color.RED);
+                button5.setTextColor(Color.BLACK);
                 plan = 19;
-                remainTextView.setText(Integer.toString(swipesLeft(plan)));
+                totalSwipes = 213;
+                swipesPerDay = 3;
+                //remainTextView.setText(Integer.toString(swipesLeft(plan)));
             }
         });
 
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                button4.setTextColor(Color.BLACK);
+                button5.setTextColor(Color.RED);
                 plan = 14;
-                remainTextView.setText(Integer.toString(swipesLeft(plan)));
+                totalSwipes = 158;
+                swipesPerDay = 2;
+                //remainTextView.setText(Integer.toString(swipesLeft(plan)));
 
+            }
+        });
+
+        btnCalculateSwipes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (swipesUsed.getText().toString().matches(""))
+                    remainTextView.setText("Enter a number of swipes and try again.");
+                else if (plan == 0)
+                    remainTextView.setText("Please select a plan and try agian.");
+                else {
+                    int intSwipesUsed = Integer.parseInt(swipesUsed.getText().toString());
+                    if ((swipesShouldHaveUsed(intSwipesUsed) - intSwipesUsed) > 0) {
+                        remainTextView.setText("You have " + Integer.toString(totalSwipes - intSwipesUsed) + " swipes and are " + Integer.toString(swipesShouldHaveUsed(intSwipesUsed) - intSwipesUsed) + " swipes ahead");
+                    }
+                    else
+                        remainTextView.setText("You have " + Integer.toString(totalSwipes - intSwipesUsed) + " swipes and are " + Integer.toString(0 - (swipesShouldHaveUsed(intSwipesUsed) - intSwipesUsed)) + " swipes behind");
+                }
             }
         });
 
